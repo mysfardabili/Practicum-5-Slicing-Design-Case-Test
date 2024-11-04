723dfa3b6b33ee15d31e5c6b4e64ed999ffe9043
mysfnearby <2300016156@webmail.uad.ac.id>
2024-11-04 22:16:11 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/**/*.html', // Menargetkan semua file HTML di dalam folder src
+    './src/**/*.js',   // Menargetkan semua file JavaScript di dalam folder src
+  ],
+  darkMode: false, // atau 'media' atau 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
